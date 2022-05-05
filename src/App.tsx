fb7626485788fe7830926dc26cc911e199f86365
Anderson Lima <andersonlimaecomp@gmail.com>
2022-05-04 23:08:36 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  // Quando se exporta sem o default, quem usa esse componente necessariamente deve usar esse nome.
+  return (
+    <div className="App">
+      <h1>Dtmoney</h1>
+    </div>
+  );
+}
