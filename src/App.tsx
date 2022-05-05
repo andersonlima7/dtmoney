37482b47d606d30ba8a802a5028b3fc026199af7
Anderson Lima <andersonlimaecomp@gmail.com>
2022-05-04 23:34:32 -0300

    First use of styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 64px;
+`;
+
 export function App() {
   // Quando se exporta sem o default, quem usa esse componente necessariamente deve usar esse nome.
   return (
     <div className="App">
-      <h1>Dtmoney</h1>
+      <Title>Dtmoney</Title>
     </div>
   );
 }
