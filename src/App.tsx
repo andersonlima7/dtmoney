55081b6581ffecc9747f3ba6640feabc38c49eb9
Anderson Lima <andersonlimaecomp@gmail.com>
2022-05-09 18:09:41 -0300

    Finish the main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import styled from "styled-components";
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-`;
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   // Quando se exporta sem o default, quem usa esse componente necessariamente deve usar esse nome.
   return (
-    <div className="App">
-      <Title>Dtmoney</Title>
-    </div>
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
   );
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./styles";
+
+export function TransactionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Salário</td>
+            <td className="deposit">+ R$ 7.000</td>
+            <td>Renda</td>
+            <td>01/06/2022</td>
+          </tr>
+          <tr>
+            <td>RX 6600</td>
+            <td className="withdraw">- R$ 2.700</td>
+            <td>Hardware</td>
+            <td>15/07/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
